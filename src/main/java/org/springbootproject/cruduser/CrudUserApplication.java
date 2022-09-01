efcf7db368b699f727a1c6a87b5394d9369e407f
Petro Pochynok <p.pochynok123@gmail.com>
2022-09-01 20:38:14 +0300

    fix typo

--- src/main/java/org/springbootproject/cruduser/CrudUserApplication.java
+++ src/main/java/org/springbootproject/cruduser/CrudUserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CruduserApplication {
+public class CrudUserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CruduserApplication.class, args);
+        SpringApplication.run(CrudUserApplication.class, args);
     }
 
 }
