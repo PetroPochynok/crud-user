6ca41560e3d8ad7752c4a208fde7d84f42e9aec8
Petro Pochynok <p.pochynok123@gmail.com>
2022-09-01 14:28:31 +0300

    initial commit

---
+++ src/main/java/org/springbootproject/curduser/cruduser/CruduserApplication.java
@@ -0,0 +1,13 @@
+package org.springbootproject.curduser.cruduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruduserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruduserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springbootproject/curduser/cruduser/CruduserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springbootproject.curduser.cruduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruduserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
