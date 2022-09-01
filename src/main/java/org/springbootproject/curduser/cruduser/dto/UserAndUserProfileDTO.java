2f2d16cea7d910e6fec42972796bbec5ceb77ac6
Petro Pochynok <p.pochynok123@gmail.com>
2022-09-01 15:53:53 +0300

    create UserAndUserProfileDTO and add mapstruct dependencies

---
+++ src/main/java/org/springbootproject/curduser/cruduser/dto/UserAndUserProfileDTO.java
@@ -0,0 +1,19 @@
+package org.springbootproject.curduser.cruduser.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserAndUserProfileDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer money;
+    private String country;
+    private String city;
+    private String street;
+}
